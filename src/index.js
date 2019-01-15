a81e014fb6d1e97e9162c3ddc6d4602bfaa851c0
carlos <[cmat27@hotmail.co]>
2019-01-14 23:15:06 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+     <div>Place Horder</div>,
+    
+    document.getElementById('root'));
+
+    serviceWorker.unregister();
